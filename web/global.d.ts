059d7f3cea3fb8de4d1399bc641badfbfd3af915
Sammy Muchiri <dev.miano@gmail.com>
2022-07-18 19:42:58 +0300

    installed project styling options

---
+++ web/global.d.ts
@@ -0,0 +1,3 @@
+declare module '@tailwindcss/forms';
+declare module '@tailwindcss/line-clamp';
+declare module '@tailwindcss/typography';
---
+++ web/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+	content: [
+		'./pages/**/*.{html, js, ts, tsx, jsx}',
+		'./components/**/*.{html, js, ts, tsx, jsx}',
+	],
+	theme: {
+		daisyui: {
+			themes: ['night'],
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Urbanist', ...defaultTheme.fontFamily.sans],
+				serif: ['Space Grotesk', ...defaultTheme.fontFamily.serif],
+				mono: ['Space Mono', ...defaultTheme.fontFamily.mono],
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography'),
+		require('daisyui'),
+	],
+};
